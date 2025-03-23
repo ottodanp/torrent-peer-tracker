a06e9d1951b4ac00b539b59a1e2c344405099daa
ottodanp <me@ottodanp.dev>
2025-03-23 13:48:26 +0000

    cleanup

--- monitor.py
+++ monitor.py
@@ -4,7 +4,36 @@ from typing import List, Dict, Any
 from aiohttp import ClientSession
 
 from db import Database
-from structures import Stats
+
+
+class Stats:
+    _total_peers_found = 0
+    _total_torrents_found = 0
+    _trackers = 0
+
+    @classmethod
+    def total_peers_found(cls):
+        return cls._total_peers_found
+
+    @classmethod
+    def total_torrents_found(cls):
+        return cls._total_torrents_found
+
+    @classmethod
+    def total_trackers_found(cls):
+        return cls._trackers
+
+    @classmethod
+    def increment_peers(cls, val: int = 1):
+        cls._total_peers_found += val
+
+    @classmethod
+    def increment_torrents(cls, val: int = 1):
+        cls._total_torrents_found += val
+
+    @classmethod
+    def increment_trackers(cls, val: int = 1):
+        cls._trackers += val
 
 
 async def get_torrents(session: ClientSession):
